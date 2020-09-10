ea3cc2c565ef0765d46cc371fdd1c7c9c78f7fe7
MichalK <kuritka@gmail.com>
2020-09-10 11:47:50 +0200

    Delete command.go

--- command.go
+++
@@ -1,9 +0,0 @@
-package shell
-
-type Command struct {
-	Command           string            // The command to run
-	Args              []string          // The args to pass to the command
-	WorkingDir        string            // The working directory
-	Env               map[string]string // Additional environment variables to set
-	OutputMaxLineSize int               // The max line size of stdout and stderr (in bytes)
-}
