b6c52f46ef442b8a7bf17fa0e9531b601ff81953
MichalK <kuritka@gmail.com>
2020-09-10 11:44:31 +0200

    Create maing.go

---
+++ maing.go
@@ -0,0 +1,15 @@
+func main() {
+
+	cmd := shell.Command{
+		Command: "terraform",
+		Args: []string{"plan", "-input=false", "-out=tfplan"},
+	}
+	o, err := shell.Execute(cmd)
+	if err != nil {
+		fmt.Printf("%v",err)
+	}
+	fmt.Println(o)
+	//output, _ := Plan()
+	//output, _ = Apply()
+	//log.Printf("AA %+v", output)
+}
